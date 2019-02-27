19c63dbe987443dab2da199f9d2dc435788d0185
Administrator <jean.pan9@qq.com>
2019-02-27 18:26:39 +0800

    -_-

--- app/cmdb/routes.py
+++ app/cmdb/routes.py
@@ -1,19 +1,18 @@
-from flask import jsonify,json
-from app.zabbix import zapi
+from flask import jsonify
+# from app.zabbix import zapi
 from app.cmdb import blueprint
 
 
 @blueprint.route('/network', methods=['GET', 'POST'])
 def testing():
-    r = zapi.hostgroup.get(output=['name'], real_hosts='true')
-    groupList = []
-    temp = []
-    for i in r:
-        temp.append(i['groupid'])
-    groupList.append({'value': temp, 'lable': '所有'})
-    print(groupList)
+    # r = zapi.hostgroup.get(output=['name'], real_hosts='true')
+    # groupList = []
+    # temp = []
+    # for i in r:
+    #     temp.append(i['groupid'])
+    # groupList.append({'value': temp, 'lable': '所有'})
     # r = zapi.host.get(output=['host'], groupids=['30', '16'])
     # print(r)
-    response = json.dumps(groupList)
-    print(response)
+    response = {'hostgroup': [{value: '1', label: 'A', 'children': []}]}
+
     return jsonify(response)
