2c8d4dc15fe5fdbd9384f99c708226e4256711a4
Administrator <jean.pan9@qq.com>
2019-02-28 17:27:15 +0800

    -_-

--- app/cmdb/routes.py
+++ app/cmdb/routes.py
@@ -10,20 +10,18 @@ def testing():
     temp = []
     list = []
 
+
+    tree = {'value': [], 'lable': '', 'children': []}
+
+
+
     for i in result:
         temp.append(i['groupid'])
         i['name'] = i['name'].split('/')
     hostgroup.append({'value': temp.copy(), 'lable': '所有'})
     temp.clear()
-
-    key = result[1]['name'][0]
-    for i in result[1:]:
-        if key == i['name'][0]:
-            temp.append(i['groupid'])
-        else:
-            list.append(i)
-    hostgroup.append({'value': temp.copy(), 'lable': ''})
-
+    
+    print(result)
     print(hostgroup)
 
     response = {
