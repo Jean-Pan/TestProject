b8e7399b54f30b72f5f6af60a6c096de9604c27b
Administrator <jean.pan9@qq.com>
2019-03-03 17:44:59 +0800

    -_-

--- app/cmdb/routes.py
+++ app/cmdb/routes.py
@@ -5,25 +5,81 @@ from app.cmdb import blueprint
 
 @blueprint.route('/network', methods=['GET', 'POST'])
 def testing():
+    class Node:
+        def __init__(self, value, label):
+            self.value = value
+            self.label = label
+            self.children = []
+
+        # def __repr__(self):
+        #     return {'value': self.value, 'label': self.label, 'children': self.children}
+
+        # def __str__(self):
+        #     return "{{'value': {}, 'label': {}, 'children': {}}}".format(self.value, self.label, self.children)
+        def turn(self):
+            return {'value': self.value, 'label': self.label, 'children': self.children}
+
+        def getValue(self):
+            return self.value
+
+        def getLabel(self):
+            return self.label
+
+        def getChild(self):
+            return self.children
+
+        def next(self, node):
+            if isinstance(self.value, str):
+                self.value = [self.value].append(node.getValue())
+            else:
+                self.value = self.value.append(node.getValue())
+            return self.children
+
+    class Tree:
+        def __init__(self, node):
+            self.head = [node]
+
+        # def __repr__(self):
+        #     return self.head
+        #
+        # def __str__(self):
+        #     return '{}'.format(self.head)
+
+        def test(self):
+            return [x.turn() for x in self.head]
+
+        def insert(self, node_list):
+            cur = self.head
+            for node in node_list:
+                if cur:
+                    for x in cur:
+                        print(1)
+                        if x and node.getLabel() == x.getLabel():
+                            print(2)
+                            cur = x.next(node)
+                        else:
+                            print(3)
+                            cur.append(node)
+                            cur = node.getChild()
+                            break
+                else:
+                    print(3)
+                    cur.append(node)
+                    cur = node.getChild()
+
     result = zapi.hostgroup.get(output=['name'], real_hosts='true')
     hostgroup = []
     temp = []
-    list = []
-
-
-    tree = {'value': [], 'lable': '', 'children': []}
-
-
 
     for i in result:
         temp.append(i['groupid'])
         i['name'] = i['name'].split('/')
-    hostgroup.append({'value': temp.copy(), 'lable': '所有'})
+    root = Node(temp.copy(), '所有')
     temp.clear()
-    
-    print(result)
-    print(hostgroup)
 
+    tree = Tree(root)
+    tree.insert([Node('a', 'test1'), Node('b', 'test2'), Node('c', 'test3')])
+    print(tree.test())
     response = {
         'hostgroup': [{'value': '1', 'label': 'A'}, {'value': '2', 'label': 'B', 'children': []}]}
 
--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-from pyzabbix import ZabbixAPI, ZabbixAPIException
+a = ['a']
 
-# 定义URL及账户密码
-zabbix_server = 'http://10.167.10.234/zabbix/api_jsonrpc.php'
-headers = {'Content-Type': 'application/json-rpc'}
-username = 'Admin'
-password = 'zabbix'
-
-zapi = ZabbixAPI(zabbix_server)
-zapi.login(username, password)
-r = zapi.problem.get(filter={'host': 'Zabbix server'})
-
-# print(r)
-
-i = zapi.problem.get(groupids='18')
-
-print(i)
-
-zapi.user.logout()
+if a:
+    for x in a:
+        print(1)
