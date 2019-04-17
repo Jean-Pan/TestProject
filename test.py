b94a386fe8a2bb12fe1c54de33f883298cd84b0f
Administrator <jean.pan9@qq.com>
2019-04-17 09:30:16 +0800

    -_-

--- test.py
+++ test.py
@@ -1,5 +1,33 @@
-a = []
-b=None
-a.append(b)
-if a:
-    print('yes')
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+
+ip = '10.167.76.5'
+community = 'public'
+oid = '1.3.6.1.2.1.17.4.3.1.1'
+
+
+def info(ip, community, oid):
+    cmdGen = cmdgen.CommandGenerator()
+    errorIndication, errorStatus, errorIndex, varBinds = cmdGen.nextCmd(
+        cmdgen.CommunityData(community),
+        cmdgen.UdpTransportTarget((ip, 161)),
+        oid
+    )
+
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print('%s at %s' % (
+            errorStatus.prettyPrint(),
+            errorIndex and varBinds[-1][int(errorIndex) - 1][0] or '?'
+        )
+              )
+    else:
+        for varBind in varBinds:
+            # print(varBind)
+            for name, val in varBind:
+                print(name.prettyPrint(), val.prettyPrint())
+        #         print(name.prettyPrint(),val)
+        #         print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+
+
+info(ip, community, oid)
